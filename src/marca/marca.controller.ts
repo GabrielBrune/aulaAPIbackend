09bcf6d7cf7d1e77ebc9f9079eded20ace450439
brune <gabriels.brunef@gmail.com>
2023-07-13 22:22:04 -0300

    aula api 13/07 att

--- src/marca/marca.controller.ts
+++ src/marca/marca.controller.ts
@@ -1,6 +1,25 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Delete, Get, Param } from "@nestjs/common";
+import { Marca } from "./marca.entity";
+import { MarcaService } from "./marca.service";
 
 @Controller('/marca')
 export class MarcaController{
-    
+    constructor(private readonly marcaServise: MarcaService){    
+    }
+
+    @Get('listar')
+    async listar(): Promise<Marca[]>{
+        return this.marcaServise.listar();
+    }
+
+    @Get('ID:id')
+    async listarID(@Param('id') id:string): Promise<Marca>{
+        return this.marcaServise.localizarID(id);
+    }
+
+    @Delete('remove-:id')
+    async removerMarca(@Param('id') id: string): Promise<void>{
+        this.marcaServise.remover(id);
+    }
+
 }
